65566ae2ec17b6574b9862e07d4d7c37e226596b
Julia Duarte Solano <juliaduartesolano@gmail.com>
2023-05-25 08:55:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,54 @@
 const app = Vue.createApp({
     data() {
-        return {
-            cart:0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
-        }
+      return {
+        cart: 0,
+        product: "Socks",
+        brand: "Vue Mastery",
+        details: ["50% cotton", "30% wool", "20% polyester"],
+        selectedVariant: 0,
+        variants: [
+            {
+                id: 2234,
+                color: "green",
+                image: "./assets/images/socks_green.jpg",
+                quantity: 50,
+              },
+              {
+                  id: 2235,
+                  color: "blue",
+                  image: "./assets/images/socks_blue.jpg",
+                  quantity: 0,
+              },
+          ],
+          onSale: true,
+      };
     },
     methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateImage(variantImage) {
-            this.image = variantImage
+      addToCart() {
+        this.cart += 1;
+      },
+      updateImage(variantImage) {
+        this.image = variantImage;
+      },
+      updateVariant(index) {
+        this.selectedVariant = index;
+      },
+    },
+    computed: {
+      title() {
+        return `${this.brand} ${this.product}`;
+      },
+      inStock() {
+        return this.variants[this.selectedVariant].quantity;
+      },
+      image() {
+        return this.variants[this.selectedVariant].image;
+      },
+      sale() {
+        if (this.onSale) {
+          return `${this.brand} ${this.product} is on sale`;
         }
-    }
-})
+        return "";
+      },
+    },
+  });
